86ad4be88b9ed5df92bc4efe68c9e11d59106df4
Robert Novotny <novotnr0@gmail.com>
2019-03-15 06:53:04 +0100

    Add GridView and FAB as instance variables

--- app/src/main/java/com/github/novotnyr/android/yello/MainActivity.java
+++ app/src/main/java/com/github/novotnyr/android/yello/MainActivity.java
@@ -1,13 +1,21 @@
 package com.github.novotnyr.android.yello;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
+    private GridView notesGridView;
+
+    private FloatingActionButton addNoteFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        notesGridView = findViewById(R.id.notesGridView);
+        addNoteFab = findViewById(R.id.addNoteFab);
     }
 }
